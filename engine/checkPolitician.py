fc5279812ddd63bd16eaee91cadf6157fd38e17d
pluca <luca.pescatore@cern.ch>
2018-11-24 11:04:57 +0100

    Improved NLP model for famous

--- engine/checkPolitician.py
+++ engine/checkPolitician.py
@@ -20,13 +20,13 @@ except Exception as e:
     sys.exit()
 
 resroot   = root+"/resources/"
-modelfile = resroot+"/resources/NLP_polititian_model.pkl"
-mapfile   = resroot+"/resources/NLP_polititian_wordmap.pkl"
+modelfile = resroot+"NLP_polititian_model.pkl"
+mapfile   = resroot+"NLP_polititian_wordmap.pkl"
 
-#trained_model = pickle.load(open(modelfile))
-#trained_word_map = pickle.load(open(mapfile))
+trained_model    = pickle.load(open(modelfile))
+trained_word_map = pickle.load(open(mapfile))
 
-
+### Splits words, simplifies them (lower case, remove stopwords, lemmatize)
 def prepareNLPData(data) :
 
     punct = list(string.punctuation)    ## Punctuation to remove
@@ -44,6 +44,7 @@ def prepareNLPData(data) :
     return lemmatised
 
 
+### Creates a dictionary mapping words to indices
 def getWordMap(toks,word_map = {}) :
 
     ### In case we need to add to a previous dictionary
@@ -57,6 +58,8 @@ def getWordMap(toks,word_map = {}) :
 
     return word_map
 
+
+### Given lemmatised tokens and a wordmap created a vector
 def tokensToVector(tokens, word_map, label = None) :
     
     dim = len(word_map)
@@ -70,6 +73,21 @@ def tokensToVector(tokens, word_map, label = None) :
     if label is not None : x[-1] = label
     return x
 
+
+### Given a list texts applies lemmatisation and creates the map
+def makeDataSet(people,word_map={}) :
+
+    ## Build map from words to vector index
+    toks = []
+    for pol in people :
+        lemms = prepareNLPData(pol)
+        word_map = getWordMap(lemms,word_map)
+        toks.append(lemms)
+    
+    return word_map, toks
+
+
+### Function to train a NLP model for classifying politicians 
 def trainNLPModel(politicians,normals) :
 
     word_map, pol_toks = makeDataSet(politicians,word_map_simple)
@@ -82,7 +100,7 @@ def trainNLPModel(politicians,normals) :
         data[i,:] = tokensToVector(toks, word_map, label = 1)
         i +=1
     for toks in norm_toks :
-        data[i,:] = tokensToVector(toks, word_map, label = 1)
+        data[i,:] = tokensToVector(toks, word_map, label = 0)
         i +=1
 
     ### Shuffle to avoid using only politicians or only normals for training
@@ -91,8 +109,8 @@ def trainNLPModel(politicians,normals) :
     features = data[:,:-1]
     labels   = data[:,-1]
 
-    #ntest = int(-0.2 * len(data))
-    ntest = 0
+    ntest = int(-0.2 * len(data))
+    #ntest = 0
     features_train = features[:ntest,]
     labels_train = labels[:ntest,]
     features_test  = features[ntest:,]
@@ -103,16 +121,16 @@ def trainNLPModel(politicians,normals) :
 
     pickle.dump(model,open(modelfile,"w"))
     pickle.dump(word_map,open(mapfile,"w"))
-    print "Classification rate"
-    #model.score(features_test,labels_test)
-    model.score(features_train,labels_train)
+    print "Classification rate", model.score(features_test,labels_test)
+    #model.score(features_train,labels_train)
 
     return model
 
 
+### Given a text return if it is a politician 
+### N.B.: Uses a pretrained model
+### N.B.: Threshold can be changed in cfg.yml
 def isPolititian(person) :
-    
-    thr = config['nlp_thr']
 
     word_map, alltoks = makeDataSet([person],trained_word_map)
     data = np.zeros((len(toks),len(word_map)))
@@ -121,13 +139,15 @@ def isPolititian(person) :
         data[i,:] = tokensToVector(toks, word_map, label = None)
         i +=1
     
+    #Code to check the weight given to each word
     #for w,i in word_map.iteritems() :
     #    weight = model.coef_[0][i]
     #    if(abs(weight)>thr) :
     #        print word, weight
-    return trained_model.predict(data)
+    return int(trained_model.predict(data) > config['nlp_thr'])
 
 
+### Calculates cosigne between to vectors as a similarity measure (could use np.dot)
 def cosvec(v1,v2) :
     
     if len(v1) != len(v2) :
@@ -147,17 +167,7 @@ def cosvec(v1,v2) :
     return float(xy) / (math.sqrt(xx)*math.sqrt(yy))
 
 
-def makeDataSet(people,word_map={}) :
-
-    ## Build map from words to vector index
-    toks = []
-    for pol in people :
-        lemms = prepareNLPData(pol)
-        word_map = getWordMap(lemms,word_map)
-        toks.append(lemms)
-    
-    return word_map, toks
-
+### Get the simplified map for these few political words to test
 def getTestMap() :
 
     politics_words = ["politics","election","decision","minister","senator","president",
@@ -173,8 +183,11 @@ def getTestMap() :
 
     return lemms, word_map_simple
 
+### Make the test objects globally available
 lemms_simple, word_map_simple = getTestMap()
 
+
+### Returns average score on a list of texts
 def scoreSimpleNLP(people) :
 
     word_map, alltoks = makeDataSet(people,word_map_simple)
@@ -185,9 +198,14 @@ def scoreSimpleNLP(people) :
 
     return np.mean(scores)
 
+
+### Returns true if the score of the simple model passes a threshold
+### N.B.: Threshold was pretrained and can be changed in cfg.yml
 def isPoliticianSimple(person) :
     return int( scoreSimpleNLP([person]) > config['simple_nlp_thr'])
 
+
+### Function to train the simplified model
 def trainSimpleNLPModel(politicians,normals) :
 
     word_map, pol_toks = makeDataSet(politicians,word_map_simple)
@@ -201,10 +219,12 @@ def trainSimpleNLPModel(politicians,normals) :
     norm_scores = [ cosvec(testvector,vec) for vec in norm_vecs ]
         
     ## The treshold will be the middle point between the two averages
+    thr = (np.mean(pol_scores) + np.mean(norm_scores))/2.
     print "Mean Politics = ", np.mean(pol_scores)
     print "Mean Normal   = ", np.mean(norm_scores)
-    print "Threshold     = ", (np.mean(pol_scores) + np.mean(norm_scores))/2.
-    return (np.mean(pol_scores) + np.mean(norm_scores))/2.
+    print "Threshold     = ", thr
+    return thr
+
 
 
 if __name__ == "__main__" :
@@ -227,10 +247,11 @@ if __name__ == "__main__" :
     
     backup = {}
 
+    ### To avoid running searches all the time can use backup
     if args.data is not None and os.path.exists(args.data) :
-    #if args.data is not None :
         backup = pickle.load(open(args.data))
         print "Loaded from saved data"
+        print backup.keys()
     
     for row in data.iterrows() :
         name         = row[1]["name"]
@@ -251,7 +272,8 @@ if __name__ == "__main__" :
         #f.close()
 
         if out != "" : backup[(name,surname)] = out
-        pickle.dump(backup,open("backup.pkl","w"))
+
+    if len(backup) > 0 : pickle.dump(backup,open("backup.pkl","w"))
     
     if args.simple : trainSimpleNLPModel(politicians,normals)
     else : trainNLPModel(politicians,normals)
--- engine/googleutils.py
+++ engine/googleutils.py
@@ -1,60 +1,51 @@
-###from lib.google_search_results import GoogleSearchResults
 import urllib, urllib2, re, requests, sys, json
 from engineutils import cleanData
 
 
-def parseGoogle(name,surname,midname="",nhits=10) :
+def parseGoogle(name,surname,midname="",country="",nhits=10) :
 
-    querytxt = '{name} {midname} {surname}'.format(
+    querytxt = '{name} {midname} {surname} {country}'.format(
                 name    = name, 
                 midname = midname,
-                surname = surname )
+                surname = surname
+                country = country ).replace("\\s+"," ")
 
     #queryurl = "https://cse.google.com/cse"
     queryurl = "https://www.googleapis.com/customsearch/v1?"
 
     f = { 
-          'cx'  : '001410688560193712145:zdxhgvqdo_o',
-          'key' : 'AIzaSyDVFkjqS8GeqzlxzRDtMgwFxQoL1wq8yOw',
-          'q'   : querytxt 
+          'cx'     : '001410688560193712145:zdxhgvqdo_o',
+          'key'    : 'AIzaSyDVFkjqS8GeqzlxzRDtMgwFxQoL1wq8yOw',
+          'lr'     : 'lang_en',
+          #'rights' : 'cc_publicdomain',
+          'num'    : nhits,
+          'q'      : querytxt 
         }
 
     print "Searching on Google"
     print querytxt
 
     myurl = queryurl+urllib.urlencode(f)
-    print myurl
     req = requests.get(myurl)
 
     results = json.loads(req.text)["items"]
 
-    #query = GoogleSearchResults({"q": querytxt})
-    #results = query.get_dictionary()
-    #if "organic_results" in results : 
-    #    results = results["organic_results"][:nhits] ## Just take first 10
-    #else :
-    #    print "Google search failed! Skipping..."
-    #    return ""
-
     fulldata = ""       ## Put all data together
+    nok = 0
     for r in results :
 
-        print "Opening", r["link"]
         try :
             response = urllib2.urlopen(r["link"])
             #headers = response.info()
             data     = response.read().decode('utf-8')
-            print data
+            if "<body" in data : nok+=1
             fulldata += data+"\n" 
-            #print data
         except : 
-            print "Skipping, something is wrong"
+            pass
+            #print "Skipping, something is wrong"
     
-    fulldata = cleanData(fulldata)
-    #print fulldata
-    #print "\n\n\n"
-
-    return fulldata
+    print "Analysed", nok, "links"
+    return cleanData(fulldata)
 
 
 
